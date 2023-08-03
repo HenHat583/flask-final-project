8f6a70cfe3f7c700ccb9c81ae32ab748b03dc59e
HenHat583 <134195656+HenHat583@users.noreply.github.com>
2023-08-03 10:24:13 +0300

    Update version-maker.py

--- version-maker.py
+++ version-maker.py
@@ -1,5 +1,6 @@
 import docker
 import re
+import os
 client = docker.from_env()
 images = client.images.list()
 # Regular expression pattern to match version numbers in the tag
@@ -17,16 +18,5 @@ else:
 # Format the version number to one decimal place
 next_version = round(next_version, 1)
 image_name = f"henhat583/flask-app:{next_version}"
-# Build the Docker image with the specified tag
-client.images.build(path=".", tag=image_name, rm=True, pull=True)
-print(f"Successfully built image: {image_name}")
-# Push the image with the specified tag to Docker Hub
-  client.images.push(repository="henhat583/flask-app", tag=next_version)
-print(f"Successfully pushed image: {image_name}")
-# Clean up older versions of the image
-for image in images:
-    if image.tags and re.match(version_pattern, image.tags[0]):
-        version = float(re.match(version_pattern, image.tags[0]).group(1))
-        if version != float(latest_version) and version != float(next_version):
-            client.images.remove(image.id, force=True)
-            print(f"Successfully removed image: {image.tags[0]}")
+with open(os.environ["GITHUB_OUTPUT"], "a") as GITHUB_OUTPUT:
+    print(f"version={image_name}", file=GITHUB_OUTPUT)
