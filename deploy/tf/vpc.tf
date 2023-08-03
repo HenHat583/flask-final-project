e0acf846c295e74f531d4eaad9fcd34610cc1f86
HenHat583 <134195656+HenHat583@users.noreply.github.com>
2023-08-04 01:33:56 +0300

    Update vpc.tf

--- deploy/tf/vpc.tf
+++ deploy/tf/vpc.tf
@@ -8,7 +8,7 @@ import {
 }
 
 resource "google_compute_network" "vpc" {
-  name                    = "${var.project_id}-vpc"
+  name = "${var.project_id}-vpc"
   auto_create_subnetworks = "false"
 }
 
