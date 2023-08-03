2245a2521f92c98d4cdbca074e246d5e7b347065
HenHat583 <134195656+HenHat583@users.noreply.github.com>
2023-08-03 14:56:04 +0300

    Create gke.tf

---
+++ deploy/tf/gke.tf
@@ -0,0 +1,12 @@
+import {
+  to = google_container_cluster.primary
+  id = "projects/planar-sunrise-393211/locations/us-central1/clusters/henhat-cluster"
+}
+
+resource "google_container_cluster" "primary" {
+  name             = "henhat-cluster"
+  location         = var.region
+  network          = google_compute_network.vpc.name
+  subnetwork       = google_compute_subnetwork.subnet.name
+  enable_autopilot = true
+}
