c5a6b01b5965efc7dda77f44186c61aee4d49a96
HenHat583 <134195656+HenHat583@users.noreply.github.com>
2023-08-05 01:27:07 +0300

    Update gke.tf

--- deploy/tf/gke.tf
+++ deploy/tf/gke.tf
@@ -1,6 +1,6 @@
 import {
   to = google_container_cluster.primary
-  id = "projects/planar-sunrise-393211/locations/us-central1-c/clusters/henhat-cluster"
+  id = "projects/planar-sunrise-393211/locations/us-central1/clusters/henhat-cluster"
 }
 
 resource "google_container_cluster" "primary" {
