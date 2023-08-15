ef9662c43c8868527a99ae9eb89a5ccec431a1d2
HenHat583 <134195656+HenHat583@users.noreply.github.com>
2023-08-15 10:47:41 +0300

    Update gke.tf

--- deploy/tf/gke.tf
+++ deploy/tf/gke.tf
@@ -6,7 +6,5 @@ import {
 resource "google_container_cluster" "primary" {
   name             = "henhat-cluster"
   location         = var.region
-  network          = google_compute_network.vpc.name
-  subnetwork       = google_compute_subnetwork.subnet.name
   enable_autopilot = true
 }
